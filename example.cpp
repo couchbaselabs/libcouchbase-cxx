879ebda104eaf6aeacde49d26034a5071834158a
signmotion <signmotion@gmail.com>
2014-09-25 19:02:20 +0300

    Extended an example for better understand the Couchbase. Added a correct JSON for key "Foo". Instead of "Warning: Editing of binary document is not allowed", we can see by http://localhost:8091 that document: {   "v": 100.1,   "list": [     1,     2,     3,     4,     5,     6,     7   ],   "talk": "About Foo." }

--- example.cpp
+++ example.cpp
@@ -13,14 +13,18 @@ using namespace Couchbase;
 
 int main(void)
 {
-    Status rv;
-    Couchbase::Client h("couchbase://localhost/default");
-    h.connect();
+    const char* url = "couchbase://localhost/default";
+    Couchbase::Client h(url);
+    Status rv = h.connect();
+    if (rv.errcode() != LCB_SUCCESS) {
+        cout << "Something wrong to connect '" << url << "'" << endl;
+        return -1;
+    }
 
     // Use a command to store an item
     StoreOperation scmd(LCB_SET);
     scmd.key("Foo");
-    scmd.value("Bar");
+    scmd.value("{ \"v\": 100.1, \"list\": [1,2,3,4,5,6,7], \"talk\": \"About Foo.\" }");
     scmd.run(h);
     Response& sres = scmd.response();
     cout << "Got status for store. Cas=" << std::hex << sres.cas() << endl;
@@ -30,7 +34,6 @@ int main(void)
     cmd.run(h);
     GetResponse& res = cmd.response();
     cout << "Got value: " << res.value() << std::endl;
-
 
     // Scheduling context. Schedule multiple operations
     BatchContext ctx(h);
@@ -57,5 +60,6 @@ int main(void)
     cout << "Value for foo is " << ctx["foo"].value() << endl;
     cout << "Value for bar is " << ctx["bar"].value() << endl;
     cout << "Value for baz is " << ctx["baz"].value() << endl;
+
     return 0;
 }
